16ced48831b85c9bc33ea2e0d6c0e77bc8e61e32
Александр <43030858+StudioColors@users.noreply.github.com>
2026-04-18 21:32:32 +0500

    Вставил ракетки на сцену
    Реализованы рабочие ракетки под контролем игрока

--- ping-pong.py
+++ ping-pong.py
@@ -3,11 +3,11 @@ from pygame import *
 #класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
 #конструктор класса
-  def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+  def __init__(self, player_image, player_x, player_y, player_speed, width, height):
       #вызываем конструктор класса (Sprite):
       sprite.Sprite.__init__(self)
       #каждый спрайт должен хранить свойство image - изображение
-      self.image = transform.scale(image.load(player_image), (size_x, size_y))
+      self.image = transform.scale(image.load(player_image), (width, height))
       self.speed = player_speed
       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
       self.rect = self.image.get_rect()
@@ -23,14 +23,14 @@ class Player(GameSprite):
        keys = key.get_pressed()
        if keys[K_UP] and self.rect.y > 5:
            self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
+       if keys[K_DOWN] and self.rect.y < win_height - 150:
            self.rect.y += self.speed
 
    def update_l(self):
        keys = key.get_pressed()
        if keys[K_w] and self.rect.y > 5:
            self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
+       if keys[K_s] and self.rect.y < win_height - 150:
            self.rect.y += self.speed
 
 #игровая сцена
@@ -60,6 +60,12 @@ while game:
     
     if finish != True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
     display.update()
     clock.tick(FPS)
