dbf02753d62341df1b7d0864d0ca84efd405377a
Александр <43030858+StudioColors@users.noreply.github.com>
2026-04-25 20:37:28 +0500

    Finished prototype
    Lose conditions implemented

--- ping-pong.py
+++ ping-pong.py
@@ -51,6 +51,11 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE', True, (180, 0, 0))
+
 speed_x = 3
 speed_y = 3
 
@@ -72,6 +77,14 @@ while game:
         
         if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
+
+        if ball.rect.x < -50:
+            finish = True
+            window.blit(lose1, (200,200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200,200))
 
         racket1.reset()
         racket2.reset()
